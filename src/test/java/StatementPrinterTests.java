08a68c96fe372cff179c6cabc489f3ca0dadb11c
amalg01 <gourriamal@gmail.com>
2023-10-23 08:56:47 +0200

    Suppression du test statementWithNewPlayTypes()

--- src/test/java/StatementPrinterTests.java
+++ src/test/java/StatementPrinterTests.java
@@ -28,21 +28,4 @@ public class StatementPrinterTests {
         verify(result);
     }
 
-    @Test
-    void statementWithNewPlayTypes() {
-
-        HashMap<String, Play> plays = new HashMap<>();
-        plays.put("henry-v",  new Play("Henry V", "history"));
-        plays.put("as-like",  new Play("As You Like It", "pastoral"));
-
-        Invoice invoice = new Invoice("BigCo", List.of(
-                new Performance("henry-v", 53),
-                new Performance("as-like", 55)));
-
-        StatementPrinter statementPrinter = new StatementPrinter();
-        Assertions.assertThrows(Error.class, () -> {
-            statementPrinter.print(invoice, plays);
-        });
-    }
-
 }
